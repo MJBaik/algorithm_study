3b4678b509ced5e1719189617d4aaf48e0315c37
MJBaik <peku9693@gmail.com>
2023-07-20 22:38:07 +0900

    230720_update

---
+++ 230720.py
@@ -0,0 +1,39 @@
+#Try1
+
+# T = int(input())
+# route = []
+
+# def bridge(N, M):
+#     global route
+#     if N == 0:
+#         return 
+#     else:
+#         for i in range(M):
+#             if M - i >= N:
+#                 route.append((N, M - i))
+#             else:
+#                 continue
+#         bridge(N-1, M-1)
+#         return N, M
+
+# for _ in range(T):
+#     N, M = map(int, inputx().split())
+#     bridge(N, M)
+#     print(len(route))
+#     route = []
+
+
+#Try2
+
+T = int(input())
+
+def factorial(x):
+    num = 1
+    for i in range (1, x+1):
+        num *= i
+    return num
+
+for test in range(T):
+    N, M = map(int, input().split())
+    route = factorial(M) // (factorial(N) * factorial(M - N))
+    print(route)
